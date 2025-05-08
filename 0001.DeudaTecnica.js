2988ac02a05bd25fc2f1a89f9f2a6434c88d9bc1
DRMoodyN <drmoodyn@gmail.com>
2025-05-07 20:04:27 -0600

    deuda tecnica

---
+++ 0001.DeudaTecnica.js
@@ -0,0 +1,30 @@
+/*
+
+CODIGO LIMPIO ES AQUEL QUE SE HA ESCRITO CON LA INTENCION DE QUE OTRA PERSONA LO ENTIENDA
+
+Que es la deuda tecnica?
+
+Falta de calidad en el codigo, que repercutira en costos futuros (Economicos).
+
+Tipos de deudas
+
+1) Imprudente: Actua de forma consiente 
+ejemplo: no hay tiempo , solo copia y pega eso de nuevo.
+
+2) Inadverido: Falta de conocimiento o falta de experiencia
+
+3) Prudente: Tenememos la deuda y estamos conciente de que existe
+
+Como se paga la deuda tecnica?
+
+Con refactorizacion el cual es un proceso que tiene como objetivo mejorar el codgo sin
+alterar su comportamiento
+
+Refactorizacion
+
+Nombres de variables
+
+Nombres para funciones
+
+Pricipio DR
+*/
